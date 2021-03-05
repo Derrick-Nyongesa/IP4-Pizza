a4948e1241a7425b94dcd5995a686324f13330bb
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-03-05 17:20:26 +0300

    add a prototype method that returns the total amount paid for the pizza depending on pizza size, crust used and toppings.

--- js/script.js
+++ js/script.js
@@ -5,3 +5,26 @@ function Pizza(size, crust) {
     this.toppings = [];
     this.price = 0;
 }
+
+Pizza.prototype.cost = function() {
+    var price = 0;
+    if(this.size == "small") {
+        price += 650;
+    } else if (this.size == "medium") {
+        price += 800;
+    } else {
+        price += 1200;
+    }
+
+    for (var i = 0; i < this.toppings.length; i++) {
+        price += 100;
+    }
+    if (this.crust == "crispy"){
+        price += 300;
+    }else if (this.crust == "stuffed") {
+        price += 250;
+    }else {
+        price += 200;
+    }
+    this.price = price;
+}
